26b79604358e664151192700495388f7c4fee2c7
Lagacione <lagacioneandre@gmail.com>
2020-05-14 10:11:56 -0300

    register screen

--- lib/screens/signup/widgets/password_field.dart
+++ lib/screens/signup/widgets/password_field.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk/generated/i18n.dart';
 
 class PasswordField extends StatelessWidget {
+
+  final FormFieldSetter<String> onSaved;
+  final bool enabled;
+  PasswordField({this.onSaved, this.enabled});
+
   @override
   Widget build(BuildContext context) {
 
@@ -20,8 +26,16 @@ class PasswordField extends StatelessWidget {
 
     return FormField<String>(
       initialValue: '',
+      onSaved: onSaved,
+      validator: (text) {
+        if (text.isEmpty || _calcStore(text) < 2)
+          return 'Senha inválida!';
+
+        return null;
+      },
       builder: (state) {
         return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             TextField(
               decoration: InputDecoration(
@@ -29,7 +43,9 @@ class PasswordField extends StatelessWidget {
               ),
               obscureText: true,
               onChanged: state.didChange,
+              enabled: enabled,
             ),
+            if (state.value.isNotEmpty)
             Container(
               margin: const EdgeInsets.only(top: 6),
               height: 8,
@@ -41,6 +57,17 @@ class PasswordField extends StatelessWidget {
                   _buildBar(3, state.value),
                 ],
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 6, left: 10),
+              child: state.value.isNotEmpty || state.hasError ? Text(
+                state.value.isNotEmpty ? _getText(_calcStore(state.value)) : state.errorText,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  color: _getColor(_calcStore(state.value)),
+                  fontSize: 12
+                ),
+              ) : Container(),
             )
           ],
         );
@@ -72,8 +99,21 @@ class PasswordField extends StatelessWidget {
         return Colors.green;
       default:
         return Colors.red;
+    }
+  }
 
-
+  String _getText(int level) {
+    switch (level) {
+      case 0:
+        return 'Senha muito fraca.';
+      case 1:
+        return 'Senha razoavelmente fraca.';
+      case 2:
+        return 'Senha razoavelmente forte.';
+      case 3:
+        return 'Senha forte.';
+      default:
+        return '';
     }
   }
 
